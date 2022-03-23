3eac7b5d0fb1ce6b477de26c66eb618cb18c0af6
Behrouz Talavari <b.talavari@persici.co.uk>
2022-03-24 00:46:48 +0430

    Microservice basked is developed

---
+++ src/Services/Basket/Basket.API/Entities/ShoppingCart.cs
@@ -0,0 +1,22 @@
+﻿namespace Basket.API.Entities
+{
+    public class ShoppingCart
+    {
+        public string UserName { get; set; }
+
+        public ShoppingCart(string userName)
+        {
+            UserName = userName;
+        }
+
+        public List<ShoppingCardItem> Items{ get; set; } = new List<ShoppingCardItem>();
+        
+        public decimal TotalPrice
+        {
+            get
+            {
+                return Items.Sum(x => x.Quantity * x.Price);
+            }
+        }
+    }
+}
