6f8aad90b00c6fd867ef7cf2c6d69d624d1aa02b
Behrouz Talavari <b.talavari@persici.co.uk>
2022-03-27 00:43:42 +0430

    add Grpc

--- src/Services/Discount/Discount.Grpc/Program.cs
+++ src/Services/Discount/Discount.Grpc/Program.cs
@@ -8,11 +8,12 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddGrpc();
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddScoped<IDiscountRepository, DiscountRepository>();
-var app = builder.Build();
-app.MigrateDatabase<Program>();
+var app = builder.Build(); 
 
+app.UseRouting();
 // Configure the HTTP request pipeline.
 app.MapGrpcService<DiscountService>();
+
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
